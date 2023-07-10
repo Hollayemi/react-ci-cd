2f77ec743b56b7da4b45b23cabe3689c8b500c65
Stephen Olayemi <stephenyemmitty@gmail.com>
2023-07-10 14:37:07 +0100

    my react ci/cd first push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <div className="App">
+      My First App
+
+      <Button variant="contained" fullWidth >MUI Button</Button>
+    </div>
+  );
+}
+
+export default App;
